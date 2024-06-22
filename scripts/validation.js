dce4e8018400a278d2a03307a06fc179537edc98
rodriguezdk11 <rodriguezdk11@gmail.com>
2024-06-21 20:31:48 -0500

    added update to validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -43,10 +43,11 @@ function checkFormValidity(inputEls) {
 
 function setEventListeners(formEl, options) {
   const { inputSelector } = options;
+  const { inputSelector, submitButtonSelector } = options;
   const inputEls = [...formEl.querySelectorAll(inputSelector)];
   const submitButton = formEl.querySelector(submitButtonSelector);
   inputEls.forEach((inputEl) => {
-    inputEl.addEventListener("input", (e) => {
+    inputEl.addEventgitListener("input", (e) => {
       checkInputValidity(formEl, inputEl, inputEls, options);
       toggleButtonState(inputEls, submitButton, options);
     });
