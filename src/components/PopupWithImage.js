bab862e2e6c2d85d5135a076f6b0019b6b68925c
rodriguezdk11 <rodriguezdk11@gmail.com>
2024-08-13 21:21:20 -0500

    update to popup modal

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,19 +3,19 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super({ popupSelector });
-    this._previewImageModal = this._popupElement.querySelector(
-      "#preview-image-modal"
-    );
+    // this._previewImagePopup = this._popupElement.querySelector(
+    //   "#preview-image-modal"
+    // );
 
     this._previewImagePopup = this._popupElement.querySelector(".modal__image");
     this._previewImageLabel = this._popupElement.querySelector(
-      "#modal__image-label"
+      ".modal__image-label"
     );
   }
 
   open({ name, link }) {
     this._previewImagePopup.src = link;
-    this._previewImagePopup.alt = name;
+    this._previewImageLabel.alt = name;
     this._previewImageLabel.textContent = name;
     super.open();
   }
